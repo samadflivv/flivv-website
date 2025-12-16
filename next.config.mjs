30a9c415d1c76721b126d4f4c5f184c2708cd0b8
Samad Khan <samadatflivv@gmail.com>
2025-12-16 14:54:33 +0530

    new deploy4

--- next.config.mjs
+++ next.config.mjs
@@ -4,23 +4,7 @@
 // export default nextConfig;
 
 
-  /** @type {import('next').NextConfig} */
-  const nextConfig = {
-    output: 'standalone',
-      images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'flivv-web-cdn.s3.ap-south-1.amazonaws.com',
-          port: '',
-          pathname: '/**',
-        },
-        // add other remote hosts here if you need them
-      ],
-    },
-  };
-
-  export default nextConfig;
+  
 
 
 // /** @type {import('next').NextConfig} */
@@ -44,3 +28,26 @@
 // };
 
 // export default nextConfig;
+
+
+
+
+
+
+/** @type {import('next').NextConfig} */
+  const nextConfig = {
+    output: 'standalone',
+      images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'flivv-web-cdn.s3.ap-south-1.amazonaws.com',
+          port: '',
+          pathname: '/**',
+        },
+        // add other remote hosts here if you need them
+      ],
+    },
+  };
+
+  export default nextConfig;
