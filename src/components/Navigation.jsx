2e56bc08e05a6af024183f84c9f683548b9603f6
Samad Khan <samadatflivv@gmail.com>
2026-01-16 16:42:58 +0530

    calendar adds2

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -166,14 +166,14 @@ export default function Navigation() {
       </li> */}
       
       {/* Hyderabad */}
-      {/* <li>
+      <li>
         <Link 
           href="/salesmeets?country=India" 
           className="block px-4 py-2 rounded-md text-black hover:bg-[#0192D3] hover:text-white mt-2"
         >
           India
         </Link>
-      </li> */}
+      </li>
     </ul>
   </div>
 </li>
@@ -392,7 +392,7 @@ export default function Navigation() {
             KSA - Riyadh
           </Link> */}
           
-          {/* <Link 
+          <Link 
             href="/salesmeets?country=India" 
             onClick={() => {
               setIsOpen(false);
@@ -401,7 +401,7 @@ export default function Navigation() {
             className="block px-6 py-4 text-lg text-gray-300 hover:bg-[#0192D3] rounded-md"
           >
             India
-          </Link> */}
+          </Link>
         </div>
       </div>
     )}
