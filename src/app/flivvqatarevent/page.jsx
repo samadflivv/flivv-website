9901f0b6cd8eef1c0cfd595ceda32ecd34dfd04d
Samad Khan <samadatflivv@gmail.com>
2026-02-28 15:36:38 +0530

    qatarpixelremove

--- src/app/flivvqatarevent/page.jsx
+++ src/app/flivvqatarevent/page.jsx
@@ -1,12 +1,10 @@
 import FlivvQatarEvent from '@/components/FlivvQatarEvent'
 import Navigation from '@/components/Navigation'
-import QatarMetaPixel from '@/components/QatarMetaPixel'
 import React from 'react'
 
 const page = () => {
   return (
     <div>
-        <QatarMetaPixel/>
         <Navigation/>
         <FlivvQatarEvent/>
     </div>
