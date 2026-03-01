eb96ba58b3d74f1ea6b5c25dd90ff7ec06378d74
Samad Khan <samadatflivv@gmail.com>
2026-03-01 14:41:20 +0530

    gmt

--- src/app/layout.js
+++ src/app/layout.js
@@ -23,6 +23,16 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-TGRWCJ9M');`}
+        </Script>
+
+
         {/* google analytics Code */}
         <Script
           async
@@ -78,6 +88,11 @@ export default function RootLayout({ children }) {
 
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+
+         {/* Google Tag Manager (noscript) */}
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TGRWCJ9M"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        
         <SmoothScrollWrapper>
           {children}
         </SmoothScrollWrapper>
