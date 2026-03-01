21c1239c95f09efba1920b0950d4ab62315764e3
Samad Khan <samadatflivv@gmail.com>
2026-03-01 14:50:39 +0530

    gmttest2

--- src/app/layout.js
+++ src/app/layout.js
@@ -90,8 +90,8 @@ export default function RootLayout({ children }) {
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
 
          {/* Google Tag Manager (noscript) */}
-        {/* <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TGRWCJ9M"
-        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript> */}
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TGRWCJ9M"
+        height="0" width="0" style={{ display: 'none', visibility: 'hidden' }}></iframe></noscript>
         
         <SmoothScrollWrapper>
           {children}
